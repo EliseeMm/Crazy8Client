1264c0687dab59deaed1772197e461404376b72a
elmathe022 <elmathe022@student.wethinkcode.co.za>
2023-10-02 12:04:53 +0200

    Initial Client

---
+++ appClient.js
@@ -0,0 +1,69 @@
+// initialize a connection with a websocket
+let ws = new WebSocket("ws://" + location.hostname + ":" + 7070 + "/crazyeight");
+
+
+// get the submit button
+const btn = document.getElementById('submit')
+
+
+
+// submit button takes the users entered name and sends it to the websocket
+// then removes the name submit form
+btn.addEventListener('click',(event) =>{
+    let name = document.getElementById('name');
+    var namev = name.value
+    const request = {
+                name: namev,
+                command : "join"
+            }
+    ws.send(JSON.stringify(request))
+
+    const form = document.getElementById('join-form')
+    event.preventDefault();  // to not refresh the page
+    form.remove() // remove the form
+
+        });
+
+
+
+
+ws.addEventListener('message', (event) => {
+    let data = JSON.parse(event.data);
+    
+    console.log(data)
+    if(data.messageType == 'gameplay'){
+
+    
+        data = {
+            cardList : data.cards,
+            centreCard : data.centreCard
+        }
+        
+
+        var template = Handlebars.compile(document.querySelector('#currentPlayerCards').innerHTML)
+        var filled = template(data);
+        document.querySelector("#output").innerHTML = filled;
+    }
+})
+// ws.onclose = () => alert("WebSocket connection closed");
+
+// // Add event listeners to button and input field
+// id("send").addEventListener("click", () => sendAndClear(id("message").value));
+// id("message").addEventListener("keypress", function (e) {
+//     if (e.keyCode === 13) { // Send message if enter is pressed in input field
+//         sendAndClear(e.target.value);
+//     }
+// });
+
+// function sendAndClear(message) {
+//     if (message !== "") {
+//         ws.send(message);
+//         id("message").value = "";
+//     }
+// }
+
+// function updateChat(msg) { // Update chat-panel and list of connected users
+//     let data = JSON.parse(msg.data);
+//     id("chat").insertAdjacentHTML("afterbegin", data.userMessage);
+//     id("userlist").innerHTML = data.userlist.map(user => "<li>" + user + "</li>").join("");
+// }
